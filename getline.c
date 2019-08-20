034337b3bf78d599d996333bf1ab852263b66049
Alb4Tr02 <834@holbertonschool.com>
2019-08-20 17:26:11 -0400

    :v

--- getline.c
+++ getline.c
@@ -1,4 +1,4 @@
-#define MAX 1024
+#define MAX 4096
 #define READING 1
 #include "holberton.h"
 #include <stdio.h>
@@ -8,21 +8,25 @@
  * @pos: pointer to the actual buffer position.
  * Return: pointer to the buffer that stores the line.
  */
-char *_getline(ssize_t *pos)
+char *_getline(ssize_t *pos, char *pathname)
 {
+	int fd;
 	ssize_t n;
 	char *cpy;
 	int state;
 	size_t size = MAX, aux;
 	char *buf = malloc(sizeof(char) * size);
-
+	if (pathname == NULL)
+		fd = 1;
+	else
+		fd = open(pathname, O_RDONLY);
 	cpy = buf;
 	state = READING;
 	while (state)
 	{
 		if (!buf)
 			return (NULL);
-		n = read(STDIN_FILENO, buf, MAX);
+		n = read(fd, buf, MAX);
 		*pos += n;
 		if (n == -1)
 		{
