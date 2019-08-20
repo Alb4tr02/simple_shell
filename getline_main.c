7b7c2f87181a9799b8e561cc13e67122f0ba7cb3
Alb4Tr02 <834@holbertonschool.com>
2019-08-20 17:23:06 -0400

    save

--- getline_main.c
+++ getline_main.c
@@ -1,3 +1,4 @@
+#define MAX 1024
 #include "holberton.h"
 #include <stdio.h>
 #include <sys/types.h>
@@ -9,16 +10,25 @@ int main (void)
 	size_t a = 0;
 	size_t *p = &a;
 	char r = 'a';
-
+	int c1 = 0, c2 = 0;
+	char *name = "/environ";
+	char *pathname = NULL;
+	char buffer[MAX];
+	pathname = getcwd(buffer, MAX);
+	for (; pathname[c1]; c1++)
+		;
+	for (; name[c2]; c2++, c1++)
+		pathname[c1] = name[c2];
+	pathname[c1] = 0;
+	int pid = getpid();
+	_getenv(pathname, pid);
 	while (1)
 	{
 		status = 0;
-		
 		a = 0;
 		char *buf = NULL;
 		prompt();
-		buf = _getline(p);
-		//printf("%s", buf);
+		buf = _getline(p, NULL);
 		command_t *h = _getargs(buf, p);
 		if (!h)
 			printf("no sirve esta mierda\n");
--- holberton.h
+++ holberton.h
@@ -2,6 +2,11 @@
 #define shelly
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #define EXT 0
 #define BUILT 1
 /**
@@ -20,11 +25,13 @@ typedef struct command_s
 } command_t;
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_getline(ssize_t *);
+char *_getline(ssize_t *pos, char *pathname);
 command_t *add_node(command_t **head, char **args, int id);
 command_t *_getargs(char *buf, ssize_t *pos);
 char **fill_nodes(char *buf, int sp, ssize_t *pos);
 int clfun(char **arg);
 int look(char *fun);
 void prompt(void);
+int _readandcpy(char *fr, char *to, char *buf);
+int _getenv(char *pathname, int pid);
 #endif
