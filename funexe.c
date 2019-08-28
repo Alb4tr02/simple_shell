86c9b2b88b98c16c09f8115cd9bd9ca7dc5f1793
Alb4Tr02 <834@holbertonschool.com>
2019-08-28 01:35:34 -0400

    exit

--- funexe.c
+++ funexe.c
@@ -153,11 +153,11 @@ int salir(command_t *h)
 	int i = 0, res = 0;
 	char *buffer = NULL;
 
-	if (h->args[1] != NULL)
+	/*if (h->args[1] != NULL)
 	{
 		res = _atoi(h->args[1]);
 		res = (res < 0) ? res * -1 : res;
-	}
+		}*/
 	buffer = getpath();
 	while (h)
 	{
